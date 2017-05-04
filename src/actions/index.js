c84c56113c42a84687e763a4e710a5b18b16ca95
galvazudys <golfas705@yahoo.com>
2017-05-04 10:33:52 +0100

    added axios and created request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,16 @@
 import API_KEY from './api';
+import axios from 'axios';
+
+const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
-export function fetchWeather(){
-    return{
-        type: FETCH_WEATHER
-    };
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`
+
+    //returns promise
+    const request = axios.get(url);
+
+    return {type: FETCH_WEATHER, payload: request};
+
 }
