9bef75e31321eb2eea87977747018d6b863dff54
galvazudys <golfas705@yahoo.com>
2017-05-04 12:52:44 +0100

    made container and table,passed data to it

---
+++ src/containers/weather_list.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+class WeatherList extends Component {
+
+    renderWeather(cityData) {
+        const temps = cityData
+            .list
+            .map(weather => weather.main.temp);
+        console.log(temps);
+        return (
+            <tr key={cityData.city.name}>
+                <td>
+                    {cityData.city.name}
+                </td>
+            </tr>
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                <table className="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>City</th>
+                            <th>Tempeture</th>
+                            <th>Pressure</th>
+                            <th>Humidity</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this
+                            .props
+                            .weather
+                            .map(this.renderWeather)}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = ({weather}) => { // replace state with {weather}
+    return {weather}; //{weather} === {weather:weather}
+}
+
+export default connect(mapStateToProps)(WeatherList);
